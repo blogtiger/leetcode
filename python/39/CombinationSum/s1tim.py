842bbb3b0a831c1edb2c2485001a4e7ca9683c39
Tim Cui <blogtiger@gmail.com>
2022-07-03 18:18:29 -0700

    39

--- python/39/CombinationSum/s1tim.py
+++ python/39/CombinationSum/s1tim.py
@@ -2,19 +2,26 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans=self.combinationSum(candidates, target, 0)
+        ans=[]
+        self.combinationSumR(candidates, target, 0, [], ans)
         return ans
         
-    def combinationSum(self, candidates: List[int], target: int, idx: int, currentset: List[int]) -> List[List[int]]:
-        if target == candidates[idx]:
-            return [*currentset, target]
+    def combinationSumR(self, candidates: List[int], target: int, idx: int, currentset: List[int], ans:List[int]) -> List[List[int]]:
+        if target<=0:
+            return
+        elif target == candidates[idx]:
+            ans.append([*currentset, target])
+        else:
+            for i in range(idx, len(candidates)):
+                currentset.append(candidates[i])
+                self.combinationSumR(candidates, target-candidates[i], i, currentset, ans)
+                currentset.pop()
+
+
         
 
 s=Solution()
 input=[2,3,6,7]
-target=4
+target=18
 ans=s.combinationSum(input, target)
 print(ans)
-print(input)
-
-print(target)
